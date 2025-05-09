9a9f1bd37071e8aef876badfd2f146e4ce9f9513
LEO-KWON1 <kojkhj614@naver.com>
2025-05-09 16:50:50 +0800

    최종 정적 배포 설정 및 코드

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  images: {
-    unoptimized: true,
-  },
   output: 'export',
   basePath: '/E_MYPROFILE',
-  assetPrefix: '/E_MYPROFILE/',
-  trailingSlash: true,
-}
-
-module.exports = nextConfig 
+  images: { unoptimized: true }
+};
+module.exports = nextConfig;
